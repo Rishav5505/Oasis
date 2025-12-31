2adeedb1282f370bd182e4e28e9a5eac63682c60
Rishav Kumar <rishavkumar33372@gmail.com>
2025-12-31 19:43:04 +0530

    Fix missing useEffect import in Navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../contexts/ThemeContext';
 import { FiSun, FiMoon, FiX } from 'react-icons/fi';
